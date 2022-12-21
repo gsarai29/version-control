8e81ba020e2f074b651b7e78a2ad8e200594dd49
Gloria Flores <gsarai.flores19@gmail.com>
2022-12-21 17:35:56 -0600

    primer commit

---
+++ src/test/java/com/flores/versioncontrol/VersionControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flores.versioncontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersionControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
